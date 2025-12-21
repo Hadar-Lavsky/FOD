6f94518f33fb8a820af9f3e9ee83d6767762b105
unknown <yurimatyash.bsc@gmail.com>
2025-12-21 22:49:10 +0200

    sorted stuff, made main

--- hyperParams.py
+++ hyperParams.py
@@ -1,4 +1,4 @@
-RAW_FOLDER = "./dataset/raw"
-MASK_FOLDER = "./dataset/mask"
-INPAINT_IMAGES_FOLDER = "./dataset/inpaint/images"
-INPAINT_LABELS_FOLDER = "./dataset/inpaint/labels"
+RAW_FOLDER = "./dataset/0_raw"
+MASK_FOLDER = "./dataset/1_mask"
+INPAINT_IMAGES_FOLDER = "./dataset/2_inpaint/images"
+INPAINT_LABELS_FOLDER = "./dataset/2_inpaint/labels"
